66bfc5ed33c856747063944449ceeeab8de18106
Flechazo <2558755403@qq.com>
2026-02-14 10:23:55 +0800

    已严肃新建文件夹

---
+++ src/main/java/cc/sighs/more_maid_interaction/MoreMaidInteraction.java
@@ -0,0 +1,12 @@
+package cc.sighs.more_maid_interaction;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(MoreMaidInteraction.MODID)
+public class MoreMaidInteraction {
+    public static final String MODID = "more_maid_interaction";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+}
